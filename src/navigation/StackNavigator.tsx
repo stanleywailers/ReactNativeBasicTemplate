b51a04fd2322f5cfe5af96ccb065e6e243e4be72
Douglas Juarez <dfjuarezreyes21@gmail.com>
2022-06-07 11:34:57 -0600

    carousel tutorial

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,25 +1,26 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
-
-
-import { TutorialScreen } from '../screens/TutorialScreen';
-import { SplashScreen } from '../screens/SplashScreen';
+import {TutorialScreen} from '../screens/TutorialScreen';
+import {SplashScreen} from '../screens/SplashScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
-
   return (
-   <Stack.Navigator
+    <Stack.Navigator
       screenOptions={{
-          headerShown: false,
-          cardStyle: {
-            backgroundColor: '#FFF'
-          }
-      }}
-   >
-    <Stack.Screen  options={{headerShown: false}} name="SplashScreen" component={SplashScreen} />
-    <Stack.Screen name="TutorialScreen" component={TutorialScreen} />
-  </Stack.Navigator>
-  )}
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#FFF',
+        },
+      }}>
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="SplashScreen"
+        component={SplashScreen}
+      />
+      <Stack.Screen name="TutorialScreen" component={TutorialScreen} />
+    </Stack.Navigator>
+  );
+};
