c7dc72fdd936a3710eaaf405790492c5cdf6e578
ricardo.melara <ricardo.melara@Novas-MacBook-Pro-2.local>
2022-07-01 11:09:35 -0600

    adjust banner

--- src/screens/tutorialScreen.tsx
+++ src/screens/tutorialScreen.tsx
@@ -1,69 +1,76 @@
-import React, { useState } from 'react'
-import { Button, Image, Modal, StyleSheet, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import React, {useState} from 'react';
+import {Button, Image, Modal, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
 
 export const TutorialScreen = () => {
+  const [modalVisible, setModalVisible] = useState(true);
 
-  const [modalVisible, setModalVisible] = useState(true)
-
+  const onPress = () => {
+   
+    setModalVisible(false)
+  };
   return (
-    <View style={styles.centeredView}>
-      <Modal style={styles.modalView}
-
-      visible={modalVisible}
-      animationType='slide'
-      
-      >
-        <View style={styles.centeredView}>
-        <TouchableOpacity style={styles.button}>
-
-          <Text style={styles.textStyle}> X</Text>
-        </TouchableOpacity>
-        <Image source={require('../assets/BienvenidaApp.png')}>
-
-
-        </Image>
-        </View>
-    
+    <View>
+      <Modal
+        style={styles.modalView}
+        visible={modalVisible}
+        animationType="slide">
+        
+          <View style={styles.centeredView}>
+            <View style={styles.button}>
+              <TouchableOpacity
+              onPress={onPress}
+              >
+                <Text style={styles.textStyle}> X</Text>
+              </TouchableOpacity>
+            </View>
+            <Image source={require('../assets/BienvenidaApp.png')}></Image>
+          </View>
+        
       </Modal>
-      
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 0,
+    backgroundColor: 'transparent',
   },
   modalView: {
     margin: 10,
-    backgroundColor: "white",
+    backgroundColor: 'yellow',
     borderRadius: 20,
     padding: 20,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
   button: {
-    borderRadius: 20,
+    overflow:'hidden',
+    backgroundColor: 'white',
+    borderRadius: 30,
     padding: 10,
-    elevation: 2
+    elevation: 2,
+    width:40,
+    height:40,
+    top: 45,
+    left: 125,
+    zIndex: 100,
   },
-  
+
   textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  }
+    color: '#00559C',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
-
-
